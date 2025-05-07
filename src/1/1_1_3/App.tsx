eca49740981510937dcc9988d55ab8bad0aeacf5
Соколова Мария <rin-marini@yandex.ru>
2025-05-07 19:27:34 +0500

    1_1_3 Fix: corrected mistake in component name

--- src/1/1_1_3/App.tsx
+++ src/1/1_1_3/App.tsx
@@ -1,26 +1,16 @@
-// 1_1_3 Spot the mistake
-/* 
-  Исправьте ошибку в компоненте Profile.
-  Вспомните, как react отличает компоненты от обычных HTML-тегов.
-*/
+function Profile() {
+  return (
+    <img src="AlanHart.jpg" alt="Alan L. Hart" />
+  );
+}
 
-function profile() {
-    return (
-      <img
-        src="AlanHart.jpg"
-        alt="Alan L. Hart"
-      />
-    );
-  }
-  
-  export default function Gallery() {
-    return (
-      <section>
-        <h1>Amazing scientists</h1>
-        <profile />
-        <profile />
-        <profile />
-      </section>
-    );
-  }
-  
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
