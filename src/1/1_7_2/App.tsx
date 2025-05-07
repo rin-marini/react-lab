ebb36dc4e9cf2d80b91483e486ca0d3dbefa0b5c
Соколова Мария <rin-marini@yandex.ru>
2025-05-07 23:29:59 +0500

    1_7_2 Feat: rendered nested lists within a single component

--- src/1/1_7_2/App.tsx
+++ src/1/1_7_2/App.tsx
@@ -1,14 +1,19 @@
-// 1_7_2 Nested lists in one component 
-/*
-  Создайте список рецептов из этого массива! Для каждого рецепта в массиве выведите его название в виде <h2> и список ингредиентов в виде <ul>. Это потребует вложения двух различных вызовов map.
-*/
-
 import { recipes } from './data';
 
 export default function RecipeList() {
-    return (
-        <div>
-            <h1>Recipes</h1>
+  return (
+    <div>
+      <h1>Recipes</h1>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h2>{recipe.name}</h2>
+          <ul>
+            {recipe.ingredients.map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
