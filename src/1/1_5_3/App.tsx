dc66c6391ae800b63b1e3921b165e9d29762d8b2
Соколова Мария <rin-marini@yandex.ru>
2025-05-07 21:59:22 +0500

    1_5_3 Feat: passed JSX content using the children prop

--- src/1/1_5_3/App.tsx
+++ src/1/1_5_3/App.tsx
@@ -1,29 +1,32 @@
-// 1_5_3 Passing JSX in a children prop
-/*
-  Извлеките компонент Card из приведенной ниже разметки и используйте параметр children для передачи ему различных JSX.
-*/
+function Card({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="card">
+      <div className="card-content">
+        {children}
+      </div>
+    </div>
+  );
+}
 
 export default function Profile() {
   return (
     <div>
-      <div className="card">
-        <div className="card-content">
-          <h1>Photo</h1>
-          <img
-            className="avatar"
-            src="OKS67lhm.jpg"
-            alt="Aklilu Lemma"
-            width={70}
-            height={70}
-          />
-        </div>
-      </div>
-      <div className="card">
-        <div className="card-content">
-          <h1>About</h1>
-          <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
-        </div>
-      </div>
+      <Card>
+        <h1>Photo</h1>
+        <img
+          className="avatar"
+          src="OKS67lhm.jpg"
+          alt="Aklilu Lemma"
+          width={70}
+          height={70}
+        />
+      </Card>
+      <Card>
+        <h1>About</h1>
+        <p>
+          Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.
+        </p>
+      </Card>
     </div>
   );
 }
