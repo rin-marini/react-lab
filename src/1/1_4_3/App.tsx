17a2f495c16cf881ce81900ac5cba8169e5e21a9
Соколова Мария <rin-marini@yandex.ru>
2025-05-07 21:17:06 +0500

    1_4_3 Fix: corrected the expression in the src attribute for the image

--- src/1/1_4_3/App.tsx
+++ src/1/1_4_3/App.tsx
@@ -1,11 +1,3 @@
-// 1_4_3 Write an expression inside JSX curly braces
-/* 
-  В приведенном ниже объекте полный URL изображения разделен на четыре части: базовый URL, imageId, imageSize и расширение файла.
-
-  Исправьте ошибку с заданием src в <img>.
-*/
-
-
 const baseUrl = '/';
 const person = {
   name: 'Gregorio Y. Zara',
@@ -23,7 +15,7 @@ export default function TodoList() {
       <h1>{person.name}'s Todos</h1>
       <img
         className="avatar"
-        src="{baseUrl}{person.imageId}{person.imageSize}.jpg"
+        src={`${baseUrl}${person.imageId}${person.imageSize}.jpg`}
         alt={person.name}
       />
       <ul>
