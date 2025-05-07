91caedb8ec78680ab00b9aca6dae618d3bf77c54
Соколова Мария <rin-marini@yandex.ru>
2025-05-07 20:09:46 +0500

    1_3_1 Refactor: converted HTML markup to valid JSX syntax

--- src/1/1_3_1/App.tsx
+++ src/1/1_3_1/App.tsx
@@ -1,17 +1,14 @@
-// 1_3_1 Convert some HTML to JSX
-/* 
-  Этот HTML был вставлен в компонент, но это не корректный JSX. Исправьте его.
-*/
-
 export default function Bio() {
-    return (
-      <div class="intro">
+  return (
+    <div className="container">
+      <div className="intro">
         <h1>Welcome to my website!</h1>
       </div>
-      <p class="summary">
+      <p className="summary">
         You can find my thoughts here.
-        <br><br>
-        <b>And <i>pictures</b></i> of scientists!
+        <br /><br />
+        <b>And <i>pictures</i></b> of scientists!
       </p>
-    );
-  }
+    </div>
+  );
+}
