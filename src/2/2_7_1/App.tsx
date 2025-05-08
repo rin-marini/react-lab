1ca413dea99567d438a185675f99bdb03bdfdb25
Соколова Мария <rin-marini@yandex.ru>
2025-05-08 20:47:27 +0500

    2_7_1 Feat: implemented item update functionality in the shopping cart

--- src/2/2_7_1/App.tsx
+++ src/2/2_7_1/App.tsx
@@ -1,49 +1,32 @@
-// 2_7_1 Update an item in the shopping cart 
-/*
-    Заполните логику handleIncreaseClick так, чтобы нажатие "+" увеличивало соответствующее число.
-*/
-
 import { useState } from 'react';
 
 const initialProducts = [
-    {
-        id: 0,
-        name: 'Baklava',
-        count: 1,
-    },
-    {
-        id: 1,
-        name: 'Cheese',
-        count: 5,
-    },
-    {
-        id: 2,
-        name: 'Spaghetti',
-        count: 2,
-    },
+  { id: 0, name: 'Baklava', count: 1 },
+  { id: 1, name: 'Cheese', count: 5 },
+  { id: 2, name: 'Spaghetti', count: 2 }
 ];
 
 export default function ShoppingCart() {
-    const [products, setProducts] = useState(
-        initialProducts
-    );
+  const [products, setProducts] = useState(initialProducts);
 
-    function handleIncreaseClick(productId: number) { }
+  function handleIncreaseClick(productId: number) { 
+    setProducts(products.map(product => 
+      product.id === productId ? { ...product, count: product.count + 1 } : product
+    ));
+  }
 
-    return (
-        <ul>
-            {products.map((product) => (
-                <li key={product.id}>
-                    {product.name} (<b>{product.count}</b>)
-                    <button
-                        onClick={() => {
-                            handleIncreaseClick(product.id);
-                        }}
-                    >
-                        +
-                    </button>
-                </li>
-            ))}
-        </ul>
-    );
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          {product.name} (<b>{product.count}</b>)
+          <button onClick={() => { 
+            handleIncreaseClick(product.id);
+          }}>
+            +
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
