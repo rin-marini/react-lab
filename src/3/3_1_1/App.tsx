ba50b448586c93dc181a810fb022b6a0114eb365
Соколова Мария <rin-marini@yandex.ru>
2025-05-08 22:41:03 +0500

    3_1_1 Feat: implemented CSS class toggling

--- src/3/3_1_1/App.tsx
+++ src/3/3_1_1/App.tsx
@@ -1,18 +1,32 @@
-// 3_1_1 Add and remove a CSS class
-/*
-    Сделайте так, чтобы щелчок на картинке удалял CSS-класс background--active из внешнего <div>, но добавлял класс picture--active к <img>. Повторный щелчок по фону восстановит исходные CSS-классы.
-
-    Визуально вы должны увидеть, что щелчок на изображении удаляет фиолетовый фон и выделяет границу изображения. Щелчок за пределами изображения выделяет фон, но убирает выделение границы изображения.
-*/
+import { useState } from 'react';
 
 export default function Picture() {
-    return (
-        <div className="background background--active">
-            <img
-                className="picture"
-                alt="Rainbow houses in Kampung Pelangi, Indonesia"
-                src="/5qwVYb1.jpg"
-            />
-        </div>
-    );
+  const [isPictureActive, setIsPictureActive] = useState(false);
+
+  function handleBackgroundClick() {
+    setIsPictureActive(false);
+  }
+
+  function handlePictureClick(e: React.MouseEvent) {
+    e.stopPropagation(); // предотвращает всплытие события на div
+    setIsPictureActive(true);
+  }
+
+  return (
+    <div
+      className={
+        'background' + (isPictureActive ? '' : ' background--active')
+      }
+      onClick={handleBackgroundClick}
+    >
+      <img
+        className={
+          'picture' + (isPictureActive ? ' picture--active' : '')
+        }
+        alt="Rainbow houses in Kampung Pelangi, Indonesia"
+        src="/5qwVYb1.jpg"
+        onClick={handlePictureClick}
+      />
+    </div>
+  );
 }
