dbbe942e62795d193cf8961dab9405bf4f5e8f69
Соколова Мария <rin-marini@yandex.ru>
2025-05-08 22:22:02 +0500

    2_7_4 Refactor: removed mutations from todo handlers using Immer

--- src/2/2_7_4/App.tsx
+++ src/2/2_7_4/App.tsx
@@ -1,60 +1,59 @@
-// 2_7_4 Fix the mutations using Immer  
-/*
-    В этом примере все обработчики событий в App.js используют мутацию. В результате редактирование и удаление todos не работает. Перепишите handleAddTodo, handleChangeTodo и handleDeleteTodo с помощью Immer
-*/
-
-import { useState } from 'react';
 import { useImmer } from 'use-immer';
 import AddTodo from './AddTodo';
 import TaskList from './TaskList';
 
 export type Todo = {
-    id: number;
-    title: string;
-    done: boolean;
+  id: number;
+  title: string;
+  done: boolean;
 }
 
 let nextId = 3;
 const initialTodos = [
-    { id: 0, title: 'Buy milk', done: true },
-    { id: 1, title: 'Eat tacos', done: false },
-    { id: 2, title: 'Brew tea', done: false },
+  { id: 0, title: 'Buy milk', done: true },
+  { id: 1, title: 'Eat tacos', done: false },
+  { id: 2, title: 'Brew tea', done: false }
 ];
 
 export default function TaskApp() {
-    const [todos, setTodos] = useState(initialTodos);
+  const [todos, setTodos] = useImmer(initialTodos);
 
-    function handleAddTodo(title: string) {
-        todos.push({
-            id: nextId++,
-            title: title,
-            done: false,
-        });
-    }
+  function handleAddTodo(title: string) {
+    setTodos(draft => {
+      draft.push({
+        id: nextId++,
+        title,
+        done: false
+      });
+    });
+  }
 
-    function handleChangeTodo(nextTodo: Todo) {
-        const todo = todos.find(
-            (t) => t.id === nextTodo.id
-        )!!;
+  function handleChangeTodo(nextTodo: Todo) {
+    setTodos(draft => {
+      const todo = draft.find(t => t.id === nextTodo.id);
+      if (todo) {
         todo.title = nextTodo.title;
         todo.done = nextTodo.done;
-    }
+      }
+    });
+  }
 
-    function handleDeleteTodo(todoId: number) {
-        const index = todos.findIndex(
-            (t) => t.id === todoId
-        );
-        todos.splice(index, 1);
-    }
+  function handleDeleteTodo(todoId: number) {
+    setTodos(draft => {
+      const index = draft.findIndex(t => t.id === todoId);
+      if (index !== -1)
+        draft.splice(index, 1);
+    });
+  }
 
-    return (
-        <>
-            <AddTodo onAddTodo={handleAddTodo} />
-            <TaskList
-                todos={todos}
-                onChangeTodo={handleChangeTodo}
-                onDeleteTodo={handleDeleteTodo}
-            />
-        </>
-    );
+  return (
+    <>
+      <AddTodo onAddTodo={handleAddTodo} />
+      <TaskList
+        todos={todos}
+        onChangeTodo={handleChangeTodo}
+        onDeleteTodo={handleDeleteTodo}
+      />
+    </>
+  );
 }
