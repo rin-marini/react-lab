27384c7309f8467235eb366fc0eb96f7de882342
Соколова Мария <rin-marini@yandex.ru>
2025-05-08 20:18:39 +0500

    2_6_2 Fix: prevented mutation of initialPosition to avoid background shift

--- src/2/2_6_2/App.tsx
+++ src/2/2_6_2/App.tsx
@@ -1,12 +1,3 @@
-// 2_6_2 Find and fix the mutation 
-/*
-    Имеется перетаскиваемый ящик на статичном фоне. Вы можете изменить цвет поля с помощью кнопки select.
-
-    Но есть ошибка. Если сначала переместить ящик, а затем изменить его цвет, фон (который не должен двигаться!) "перепрыгнет" на позицию ящика. Но этого не должно произойти: параметр position у Background установлен в initialPosition, что равно { x: 0, y: 0 }. Почему фон перемещается после изменения цвета?
-
-    Найдите ошибку и исправьте ее.
-*/
-
 import { useState } from 'react';
 import Background from './Background';
 import Box from './Box';
@@ -14,46 +5,41 @@ import Box from './Box';
 export type Position = { x: number; y: number };
 
 const initialPosition = {
-    x: 0,
-    y: 0,
+  x: 0,
+  y: 0
 };
 
 export default function Canvas() {
-    const [shape, setShape] = useState({
-        color: 'orange',
-        position: initialPosition,
+  const [shape, setShape] = useState({
+    color: 'orange',
+    position: initialPosition
+  });
+
+  function handleMove(dx: number, dy: number) {
+    setShape({
+      ...shape,
+      position: {x: shape.position.x + dx, y: shape.position.y + dy}
     });
+  }
 
-    function handleMove(dx: number, dy: number) {
-        shape.position.x += dx;
-        shape.position.y += dy;
-    }
+  function handleColorChange(e: any) {
+    setShape({
+      ...shape,
+      color: e.target.value
+    });
+  }
 
-    function handleColorChange(e: any) {
-        setShape({
-            ...shape,
-            color: e.target.value,
-        });
-    }
-
-    return (
-        <>
-            <select
-                value={shape.color}
-                onChange={handleColorChange}
-            >
-                <option value="orange">orange</option>
-                <option value="lightpink">lightpink</option>
-                <option value="aliceblue">aliceblue</option>
-            </select>
-            <Background position={initialPosition} />
-            <Box
-                color={shape.color}
-                position={shape.position}
-                onMove={handleMove}
-            >
-                Drag me!
-            </Box>
-        </>
-    );
+  return (
+    <>
+      <select value={shape.color} onChange={handleColorChange}>
+        <option value="orange">orange</option>
+        <option value="lightpink">lightpink</option>
+        <option value="aliceblue">aliceblue</option>
+      </select>
+      <Background position={initialPosition} />
+      <Box color={shape.color} position={shape.position} onMove={handleMove}>
+        Drag me!
+      </Box>
+    </>
+  );
 }
