ae373eb4a0f926fc03f8fadb99cc78741a3e2c55
Соколова Мария <rin-marini@yandex.ru>
2025-05-08 21:56:07 +0500

    2_7_3 Refactor: removed mutations from todo handlers for immutability

--- src/2/2_7_3/App.tsx
+++ src/2/2_7_3/App.tsx
@@ -1,59 +1,46 @@
-// 2_7_3 Fix the mutations using non-mutative methods  
-/*
-    В этом примере все обработчики событий в App.js используют мутацию. В результате редактирование и удаление todos не работает. Перепишите handleAddTodo, handleChangeTodo и handleDeleteTodo, чтобы они использовали немутационные методы:
-*/
-
 import { useState } from 'react';
 import AddTodo from './AddTodo';
 import TaskList from './TaskList';
 
 export type Todo = {
-    id: number;
-    title: string;
-    done: boolean;
+  id: number;
+  title: string;
+  done: boolean;
 }
 
 let nextId = 3;
 const initialTodos = [
-    { id: 0, title: 'Buy milk', done: true },
-    { id: 1, title: 'Eat tacos', done: false },
-    { id: 2, title: 'Brew tea', done: false },
+  { id: 0, title: 'Buy milk', done: true },
+  { id: 1, title: 'Eat tacos', done: false },
+  { id: 2, title: 'Brew tea', done: false }
 ];
 
 export default function TaskApp() {
-    const [todos, setTodos] = useState(initialTodos);
+  const [todos, setTodos] = useState(initialTodos);
 
-    function handleAddTodo(title: string) {
-        todos.push({
-            id: nextId++,
-            title: title,
-            done: false,
-        });
-    }
+  function handleAddTodo(title: string) {
+    setTodos([
+      ...todos,
+      { id: nextId++, title: title, done: false }
+    ]);
+  }
 
-    function handleChangeTodo(nextTodo: Todo) {
-        const todo = todos.find(
-            (t) => t.id === nextTodo.id
-        )!!;
-        todo.title = nextTodo.title;
-        todo.done = nextTodo.done;
-    }
+  function handleChangeTodo(nextTodo: Todo) {
+    setTodos(todos.map(todo => todo.id === nextTodo.id ? nextTodo : todo));
+  }
 
-    function handleDeleteTodo(todoId: number) {
-        const index = todos.findIndex(
-            (t) => t.id === todoId
-        );
-        todos.splice(index, 1);
-    }
+  function handleDeleteTodo(todoId: number) {
+    setTodos(todos.filter(todo => todo.id !== todoId));
+  }
 
-    return (
-        <>
-            <AddTodo onAddTodo={handleAddTodo} />
-            <TaskList
-                todos={todos}
-                onChangeTodo={handleChangeTodo}
-                onDeleteTodo={handleDeleteTodo}
-            />
-        </>
-    );
+  return (
+    <>
+      <AddTodo onAddTodo={handleAddTodo} />
+      <TaskList
+        todos={todos}
+        onChangeTodo={handleChangeTodo}
+        onDeleteTodo={handleDeleteTodo}
+      />
+    </>
+  );
 }
