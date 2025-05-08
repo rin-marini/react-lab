efa90b935a9b3ecb451305a95e3289a04da73527
Соколова Мария <rin-marini@yandex.ru>
2025-05-08 17:36:10 +0500

    2_2_1 Feat: added "Previous" button and prevented index out-of-bounds in Gallery

--- src/2/2_2_1/App.tsx
+++ src/2/2_2_1/App.tsx
@@ -1,42 +1,35 @@
-// 2_2_1 Complete the gallery 
-/*
-  При нажатии кнопки "Next" на последней скульптуре происходит сбой кода. Исправьте логику, чтобы предотвратить сбой. Вы можете сделать это, добавив дополнительную логику в обработчик события или отключив кнопку, когда действие невозможно.
-
-  После устранения сбоя добавьте кнопку "Предыдущая", которая показывает предыдущую скульптуру. Она не должна разбиваться на первой скульптуре.
-*/
-
 import { useState } from 'react';
 import { sculptureList } from './data';
 
 
 export default function Gallery() {
-    const [index, setIndex] = useState(0);
-    const [showMore, setShowMore] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
 
-    function handleNextClick() {
-        setIndex(index + 1);
-    }
+  function handleNextClick() {
+    if (index < sculptureList.length - 1)
+      setIndex(index + 1);
+  }
 
-    function handleMoreClick() {
-        setShowMore(!showMore);
-    }
+  function handlePrevClick() {
+    if (index > 0) 
+      setIndex(index - 1);
+  }
 
-    let sculpture = sculptureList[index];
-    return (
-        <>
-            <button onClick={handleNextClick}>Next</button>
-            <h2>
-                <i>{sculpture.name} </i>
-                by {sculpture.artist}
-            </h2>
-            <h3>
-                ({index + 1} of {sculptureList.length})
-            </h3>
-            <button onClick={handleMoreClick}>
-                {showMore ? 'Hide' : 'Show'} details
-            </button>
-            {showMore && <p>{sculpture.description}</p>}
-            <img src={sculpture.url} alt={sculpture.alt} />
-        </>
-    );
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
+  return (
+    <>
+      <button onClick={handlePrevClick} disabled={index === 0}>Previous</button>
+      <button onClick={handleNextClick} disabled={index === sculptureList.length - 1}>Next</button>
+      <h2><i>{sculpture.name}</i> by {sculpture.artist}</h2>
+      <h3>({index + 1} of {sculptureList.length})</h3>
+      <button onClick={handleMoreClick}>{showMore ? 'Hide' : 'Show'} details</button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
+    </>
+  );
 }
