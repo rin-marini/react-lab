addff4cc6bbec4f9600ad6222b1db6117fdf5a3c
Соколова Мария <rin-marini@yandex.ru>
2025-05-09 12:45:15 +0500

    3_2_1 Fix: resolved issue with component not updating

--- src/3/3_2_1/Clock.tsx
+++ src/3/3_2_1/Clock.tsx
@@ -1,9 +1,6 @@
-import { useState } from 'react';
-
 export default function Clock(props: { time: string, color: string }) {
-  const [color, setColor] = useState(props.color);
   return (
-    <h1 style={{ color: color }}>
+    <h1 style={{ color: props.color }}>
       {props.time}
     </h1>
   );
