1dbd7d4a4c15e3942c3e7971d0a90ddef51b3a1a
Соколова Мария <rin-marini@yandex.ru>
2025-05-09 16:59:51 +0500

    3_5_1 Feat: dispatched actions from event handlers for state management

--- src/3/3_5_1/Chat.tsx
+++ src/3/3_5_1/Chat.tsx
@@ -1,25 +1,25 @@
-import { useState } from 'react';
 import { Action } from './messengerReducer';
 import { Contact } from './App';
 
-export default function Chat(
-    {
-        contact, message, dispatch
-    }:
-    {
-        contact: Contact,
-        message: string,
-        dispatch: (action: Action) => void
-    }
-) {
+export default function Chat({
+  contact, 
+  message, 
+  dispatch
+} : {
+  contact: Contact,
+  message: string,
+  dispatch: (action: Action) => void
+}) {
   return (
     <section className="chat">
       <textarea
         value={message}
         placeholder={'Chat to ' + contact.name}
         onChange={(e) => {
-          // TODO: dispatch edited_message
-          // (Read the input value from e.target.value)
+          dispatch({
+            type: 'edited_message',
+            message: e.target.value
+          });
         }}
       />
       <br />
