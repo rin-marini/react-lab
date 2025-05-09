3516b56b4791e5ddcd93005b2008b7db77c6a52c
Соколова Мария <rin-marini@yandex.ru>
2025-05-09 14:22:48 +0500

    3_4_1 Fix: prevented input text from disappearing when toggling hint visibility

--- src/3/3_4_1/App.tsx
+++ src/3/3_4_1/App.tsx
@@ -1,35 +1,31 @@
-// 3_4_1 Fix disappearing input text 
-/*
-  Этот пример показывает сообщение при нажатии на кнопку. Однако при нажатии кнопки также происходит случайный сброс ввода. Почему так происходит? Исправьте, чтобы нажатие кнопки не сбрасывало вводимый текст.
-*/
-
 import { useState } from 'react';
 
 export default function App() {
   const [showHint, setShowHint] = useState(false);
-  if (showHint) {
-    return (
-      <div>
-        <p><i>Hint: Your favorite city?</i></p>
-        <Form />
-        <button onClick={() => {
-          setShowHint(false);
-        }}>Hide hint</button>
-      </div>
-    );
-  }
+  const [text, setText] = useState(''); // Переносим состояние сюда
+
   return (
     <div>
-      <Form />
+      {showHint && (
+        <p><i>Hint: Your favorite city?</i></p>
+      )}
+      <Form text={text} setText={setText} />
       <button onClick={() => {
-        setShowHint(true);
-      }}>Show hint</button>
+        setShowHint(!showHint);
+      }}>
+        {showHint ? 'Hide hint' : 'Show hint'}
+      </button>
     </div>
   );
 }
 
-function Form() {
-  const [text, setText] = useState('');
+function Form({
+  text,
+  setText
+}: {
+  text: string,
+  setText: (text: string) => void
+}) {
   return (
     <textarea
       value={text}
@@ -37,4 +33,3 @@ function Form() {
     />
   );
 }
-
