a7febb32b4973170fd14157825928ec89d621a42
Соколова Мария <rin-marini@yandex.ru>
2025-05-09 18:00:27 +0500

    3_5_3 Fix: restored input values when switching between chats

--- src/3/3_5_3/App.tsx
+++ src/3/3_5_3/App.tsx
@@ -1,11 +1,3 @@
-// 3_5_3  Restore input values when switching
-/*
-  В этом примере переключение между разными получателями всегда очищает текстовый ввод. Это связано с тем, что вы не хотите разделять черновик одного сообщения между несколькими получателями. Но было бы лучше, если бы ваше приложение "запоминало" черновики для каждого контакта отдельно, восстанавливая их при переключении контактов.
-
-  Ваша задача - изменить структуру состояния таким образом, чтобы запоминать  отдельный черновик сообщения для каждого контакта. Вам потребуется внести несколько изменений в редуктор, начальное состояние и компоненты.
-    
-*/
-
 import { useReducer } from 'react';
 import Chat from './Chat';
 import ContactList from './ContactList';
@@ -13,7 +5,7 @@ import { initialState, messengerReducer } from './messengerReducer';
 
 export default function Messenger() {
   const [state, dispatch] = useReducer(messengerReducer, initialState);
-  const message = state.message;
+  const message = state.messages[state.selectedId] ?? '';
   const contact = contacts.find((c) => c.id === state.selectedId)!!;
   return (
     <div>
@@ -41,5 +33,5 @@ export type Contact = {
 const contacts = [
   {id: 0, name: 'Taylor', email: 'taylor@mail.com'},
   {id: 1, name: 'Alice', email: 'alice@mail.com'},
-  {id: 2, name: 'Bob', email: 'bob@mail.com'},
+  {id: 2, name: 'Bob', email: 'bob@mail.com'}
 ];
--- src/3/3_5_3/messengerReducer.ts
+++ src/3/3_5_3/messengerReducer.ts
@@ -1,49 +1,50 @@
 export type State = {
-    selectedId: number;
-    message: string;
+  selectedId: number;
+  messages: { [contactId: number]: string };
 };
 
-export type Action = {
-    type: 'changed_selection';
-    contactId: number;
-} | {
-    type: 'edited_message';
-    message: string;
-} | {
-    type: 'sent_message';
+export type Action = 
+  | { type: 'changed_selection'; contactId: number }
+  | { type: 'edited_message'; message: string }
+  | { type: 'sent_message' };
+
+export const initialState: State = {
+  selectedId: 0,
+  messages: {
+    0: 'Hello',
+    1: '',
+    2: ''
+  }
 };
 
-export const initialState = {
-    selectedId: 0,
-    message: 'Hello',
-};
-
-export function messengerReducer(
-    state: State,
-    action: Action
-) {
-    switch (action.type) {
-        case 'changed_selection': {
-            return {
-                ...state,
-                selectedId: action.contactId,
-                message: '',
-            };
-        }
-        case 'edited_message': {
-            return {
-                ...state,
-                message: action.message,
-            };
-        }
-        case 'sent_message': {
-            return {
-              ...state,
-              message: '',
-            };
-          }        
-        default: {
-            throw Error('Unknown action: ' + action.type);
-        }
+export function messengerReducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'changed_selection': {
+      return {
+        ...state,
+        selectedId: action.contactId
+      };
     }
+    case 'edited_message': {
+      return {
+        ...state,
+        messages: {
+          ...state.messages,
+          [state.selectedId]: action.message
+        }
+      };
+    }
+    case 'sent_message': {
+      return {
+        ...state,
+        messages: {
+          ...state.messages,
+          [state.selectedId]: ''
+        }
+      };
+    }
+    default: {
+      throw Error('Unknown action: ' + action.type);
+    }
+  }
 }
