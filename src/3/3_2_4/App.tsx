528fb2844b34a83e5af2b27a8eafbd594ae57680
Соколова Мария <rin-marini@yandex.ru>
2025-05-09 13:35:21 +0500

    3_2_4 Feat: implement multiple selection

--- src/3/3_2_4/App.tsx
+++ src/3/3_2_4/App.tsx
@@ -1,24 +1,19 @@
-// 3_2_4 Implement multiple selection
-/*
-    В этом примере каждое Letter имеет свойство isSelected и обработчик onToggle, который отмечает его как выбранное. Это работает, но состояние хранится как selectedId (либо null, либо ID), поэтому в каждый момент времени может быть выбрано только одно письмо.
-
-    Измените структуру состояния для поддержки множественного выбора (Как бы вы его структурировали? Подумайте об этом перед написанием кода). Каждый флажок должен стать независимым от других. Щелчок по выбранному письму должен снимать флажок. Наконец, нижний колонтитул должен показывать правильное количество выбранных элементов.
-*/
-
-
 import { useState } from 'react';
 import { letters } from './data.js';
 import Letter from './Letter.js';
 
 export default function MailClient() {
-  const [selectedId, setSelectedId] = useState<number | null>(null);
+  const [selectedIds, setSelectedIds] = useState<number[]>([]);
 
-  // TODO: allow multiple selection
-  const selectedCount = 1;
+  const selectedCount = selectedIds.length;
 
   function handleToggle(toggledId: number) {
-    // TODO: allow multiple selection
-    setSelectedId(toggledId);
+    setSelectedIds(prevSelected => {
+      if (prevSelected.includes(toggledId)) 
+        return prevSelected.filter(id => id !== toggledId);
+      else
+        return [...prevSelected, toggledId];
+    });
   }
 
   return (
@@ -29,10 +24,7 @@ export default function MailClient() {
           <Letter
             key={letter.id}
             letter={letter}
-            isSelected={
-              // TODO: allow multiple selection
-              letter.id === selectedId
-            }
+            isSelected={selectedIds.includes(letter.id)}
             onToggle={handleToggle}
           />
         ))}
