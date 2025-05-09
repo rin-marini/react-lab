7c9a6eb3fe9f64f2ce93d37b7a6ca69250cee645
Соколова Мария <rin-marini@yandex.ru>
2025-05-09 18:52:02 +0500

    3_6_1 Refactor: replace prop drilling with context

--- src/3/3_6_1/App.tsx
+++ src/3/3_6_1/App.tsx
@@ -1,59 +1,41 @@
-// 3_6_1  Replace prop drilling with context
-/*
-  В этом примере переключение флажка изменяет параметр imageSize, передаваемый каждому <PlaceImage>. Состояние флажка хранится в компоненте верхнего уровня App, но каждый <PlaceImage> должен знать об этом.
-
-  В настоящее время App передает imageSize в List, который передает его в каждое Place, которое передает его в PlaceImage. Удалите пропс imageSize, и вместо этого передавайте его из компонента App непосредственно в PlaceImage.
-
-  Вы можете объявить контекст в файле Context.js.
-*/
-
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { places, PlaceType } from './data';
 import { getImageUrl } from './utils';
+import { Context } from './Context';
 
 export default function App() {
   const [isLarge, setIsLarge] = useState(false);
   const imageSize = isLarge ? 150 : 100;
+
   return (
-    <>
+    <Context.Provider value={imageSize}>
       <label>
         <input
           type="checkbox"
           checked={isLarge}
-          onChange={e => {
-            setIsLarge(e.target.checked);
-          }}
+          onChange={e => setIsLarge(e.target.checked)}
         />
         Use large images
       </label>
       <hr />
-      <List imageSize={imageSize} />
-    </>
-  )
+      <List />
+    </Context.Provider>
+  );
 }
 
-function List({ imageSize }: { imageSize: number }) {
+function List() {
   const listItems = places.map(place =>
     <li key={place.id}>
-      <Place
-        place={place}
-        imageSize={imageSize}
-      />
+      <Place place={place} />
     </li>
   );
   return <ul>{listItems}</ul>;
 }
 
-function Place(
-  { place, imageSize }: 
-  { place: PlaceType, imageSize: number }
-) {
+function Place({ place }: { place: PlaceType }) {
   return (
     <>
-      <PlaceImage
-        place={place}
-        imageSize={imageSize}
-      />
+      <PlaceImage place={place} />
       <p>
         <b>{place.name}</b>
         {': ' + place.description}
@@ -62,10 +44,9 @@ function Place(
   );
 }
 
-function PlaceImage(
-  { place, imageSize }:
-    { place: PlaceType, imageSize: number }
-) {
+function PlaceImage({ place } : { place: PlaceType }) {
+  const imageSize = useContext(Context);
+
   return (
     <img
       src={getImageUrl(place)}
