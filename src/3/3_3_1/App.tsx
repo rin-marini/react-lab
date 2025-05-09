66722bbe00a6f0756a1af09137f0f7f4da2422ca
Соколова Мария <rin-marini@yandex.ru>
2025-05-09 13:46:48 +0500

    3_3_1 Feat: implemented synced input fields

--- src/3/3_3_1/App.tsx
+++ src/3/3_3_1/App.tsx
@@ -1,30 +1,32 @@
-// 3_3_1 Synced inputs 
-/*
-  Эти два входа являются независимыми. Сделайте их синхронизированными: редактирование одного входа должно обновить другой вход с тем же текстом, и наоборот.
-*/
-
 import { useState } from 'react';
 
 export default function SyncedInputs() {
+  const [text, setText] = useState('');
+
   return (
     <>
-      <Input label="First input" />
-      <Input label="Second input" />
+      <Input label="First input" text={text} setText={setText} />
+      <Input label="Second input" text={text} setText={setText} />
     </>
   );
 }
 
-function Input({ label }: { label: string }) {
-  const [text, setText] = useState('');
-
-  function handleChange(e) {
+function Input({
+  label,
+  text,
+  setText
+}: {
+  label: string;
+  text: string;
+  setText: (value: string) => void;
+}) {
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setText(e.target.value);
   }
 
   return (
     <label>
-      {label}
-      {' '}
+      {label}{' '}
       <input
         value={text}
         onChange={handleChange}
