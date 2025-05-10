b88e1e36160a773cead0af3113ad35122b7b6fe8
Соколова Мария <rin-marini@yandex.ru>
2025-05-10 15:29:49 +0500

    4_3_3 Fix: prevented double triggering of setInterval

--- src/4/4_3_3/Counter.tsx
+++ src/4/4_3_3/Counter.tsx
@@ -8,7 +8,9 @@ export default function Counter() {
       setCount(c => c + 1);
     }
 
-    setInterval(onTick, 1000);
+    const intervalId = setInterval(onTick, 1000);
+
+    return () => clearInterval(intervalId); 
   }, []);
 
   return <h1>{count}</h1>;
