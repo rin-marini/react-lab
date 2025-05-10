a41e569d63cc639e3fdfac04bd4f9831947980d2
Соколова Мария <rin-marini@yandex.ru>
2025-05-10 14:24:50 +0500

    4_2_2 Feat: set focus on input field when button is clicked

--- src/4/4_2_2/App.tsx
+++ src/4/4_2_2/App.tsx
@@ -1,15 +1,17 @@
-// 4_2_2 Focus the search field
-/*
-  Сделайте так, чтобы нажатие на кнопку "Поиск" наводило фокус на поле.
-*/
+import { useRef } from 'react';
 
 export default function Page() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
     <>
       <nav>
-        <button>Search</button>
+        <button onClick={() => inputRef.current?.focus()}>
+          Search
+        </button>
       </nav>
       <input
+        ref={inputRef}
         placeholder="Looking for something?"
       />
     </>
