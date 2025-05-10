9bfbfdae64afa55d6c4693a944144f2dd52d51c8
Соколова Мария <rin-marini@yandex.ru>
2025-05-10 22:24:16 +0500

    4_5_5 Feat: implemented correct population of dependent dropdown list

--- src/4/4_5_5/App.tsx
+++ src/4/4_5_5/App.tsx
@@ -1,12 +1,3 @@
-// 4_5_5 Populate a chain of select boxes
-/*
-  В этом примере есть два поля выбора. Одно поле позволяет пользователю выбрать планету. Другое поле позволяет пользователю выбрать место на этой планете. Второе поле пока не работает. Ваша задача - заставить его показывать места на выбранной планете.
-
-  Посмотрите, как работает первое поле выбора. Оно заполняет состояние planetList результатом вызова API "/planets". ID выбранной планеты хранится в переменной состояния planetId. Вам нужно найти, куда добавить дополнительный код, чтобы переменная состояния placeList заполнялась результатом вызова API "/planets/" + planetId + "/places".
-
-  Если вы реализуете это правильно, выбор планеты должен заполнить список мест. Изменение планеты должно изменить список мест.
-*/
-
 import { useState, useEffect } from 'react';
 import { fetchData, GeoObj } from './api';
 
@@ -30,6 +21,21 @@ export default function Page() {
       ignore = true;
     }
   }, []);
+
+  useEffect(() => {
+    if (!planetId) return;
+    let ignore = false;
+    fetchData(`/planets/${planetId}/places`).then(result => {
+      if (!ignore) {
+        console.log(`Fetched places for planet ${planetId}.`);
+        setPlaceList(result);
+        setPlaceId(result[0]?.id || '');
+      }
+    });
+    return () => {
+      ignore = true;
+    };
+  }, [planetId]);
 
   return (
     <>
