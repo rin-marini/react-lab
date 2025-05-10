1332a73bed01a959c1651fd09899c5e2bf5b079b
Соколова Мария <rin-marini@yandex.ru>
2025-05-10 12:13:26 +0500

    4_1_1 Fix: allowed message sending to be canceled properly

--- src/4/4_1_1/App.tsx
+++ src/4/4_1_1/App.tsx
@@ -1,19 +1,13 @@
-// 4_1_1  Fix a broken chat input
-/*
-  Если ввести сообщение и нажать "Отправить" то перед появлением сообщения "Отправлено!" произойдет трехсекундная задержка. Кнопка "Отменить" должна остановить появление сообщения "Отправлено!". Она делает это, вызывая clearTimeout для идентификатора таймаута, сохраненного во время handleSend. Однако даже после нажатия кнопки "Отменить" сообщение "Отправлено!" все равно появляется. Найдите причину неработоспособности и устраните ее.
-*/
-
-import { useState } from 'react';
-import { getImageUrl } from "./utils"
+import { useRef, useState } from 'react';
 
 export default function Chat() {
   const [text, setText] = useState('');
   const [isSending, setIsSending] = useState(false);
-  let timeoutID = null;
+  const timeoutID = useRef<number | null>(null);
 
   function handleSend() {
     setIsSending(true);
-    timeoutID = setTimeout(() => {
+    timeoutID.current = window.setTimeout(() => {
       alert('Отправлено!');
       setIsSending(false);
     }, 3000);
@@ -21,7 +15,9 @@ export default function Chat() {
 
   function handleUndo() {
     setIsSending(false);
-    clearTimeout(timeoutID);
+    if (timeoutID.current !== null) {
+      clearTimeout(timeoutID.current);
+    }
   }
 
   return (
