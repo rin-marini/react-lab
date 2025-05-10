351ed84846099bace92376185dd6216b7cba739d
Соколова Мария <rin-marini@yandex.ru>
2025-05-10 15:14:57 +0500

    4_3_2 Feat: added conditional focus logic to MyInput component

--- src/4/4_3_2/MyInput.tsx
+++ src/4/4_3_2/MyInput.tsx
@@ -1,21 +1,20 @@
 import { useEffect, useRef } from 'react';
 
-export default function MyInput(
-    { 
-        shouldFocus, 
-        value, 
-        onChange 
-    }: {
-        shouldFocus: boolean,
-        value: string,
-        onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
-    }) {
+export default function MyInput({ 
+  shouldFocus, 
+  value, 
+  onChange 
+} : {
+  shouldFocus: boolean,
+  value: string,
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+}) {
   const ref = useRef<HTMLInputElement>(null);
 
-  // TODO: call focus() only if shouldFocus is true.
   useEffect(() => {
-    ref.current?.focus();
-  }, []);
+    if (shouldFocus)
+      ref.current?.focus();
+  }, [shouldFocus]);
 
   return (
     <input
