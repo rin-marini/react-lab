16e3583e8fc73cb3fa03fbe885ea1d1c1cd7b0fe
Соколова Мария <rin-marini@yandex.ru>
2025-05-11 00:08:37 +0500

    4_8_1 Refactor: extract a useCounter Hook

--- src/4/4_8_1/App.tsx
+++ src/4/4_8_1/App.tsx
@@ -1,25 +1,6 @@
-// 4_8_1 Extract a useCounter Hook
-/*
-  Этот компонент использует переменную состояния и Эффект для отображения числа, которое увеличивается каждую секунду. Извлеките эту логику в пользовательский хук под названием useCounter. Ваша цель состоит в том, чтобы реализация компонента Counter выглядела именно так:
-  
-  import { useCounter } from "./useCounter";
-
-  export default function Counter() {
-    const count = useCounter();
-    return <h1>Seconds passed: {count}</h1>;
-  }
-
-*/
-
-import { useState, useEffect } from 'react';
+import { useCounter } from "./useCounter";
 
 export default function Counter() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    const id = setInterval(() => {
-      setCount(c => c + 1);
-    }, 1000);
-    return () => clearInterval(id);
-  }, []);
+  const count = useCounter();
   return <h1>Seconds passed: {count}</h1>;
 }
