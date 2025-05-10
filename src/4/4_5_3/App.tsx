6d7da9150f3201a2ba81338a71e636e5a9129ee4
Соколова Мария <rin-marini@yandex.ru>
2025-05-10 21:27:08 +0500

    4_5_3 Fix: resolved stale value issue in handleMove

--- src/4/4_5_3/App.tsx
+++ src/4/4_5_3/App.tsx
@@ -1,27 +1,17 @@
-// 4_5_3 Investigate a stale value bug
-/*
-  В этом примере розовая точка должна двигаться, когда флажок включен, и прекращать движение, когда флажок выключен. Логика для этого уже реализована: обработчик события handleMove проверяет переменную состояния canMove.
-
-  Однако по какой-то причине переменная состояния canMove внутри handleMove кажется "несвежей": она всегда true, даже после того, как вы установили флажок. Как такое возможно? Найдите ошибку в коде и исправьте ее.
-*/
-
 import { useState, useEffect } from 'react';
 
 export default function App() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [canMove, setCanMove] = useState(true);
 
-  function handleMove(e) {
-    if (canMove) {
-      setPosition({ x: e.clientX, y: e.clientY });
-    }
-  }
-
   useEffect(() => {
+    function handleMove(e: PointerEvent) {
+      if (canMove)
+        setPosition({ x: e.clientX, y: e.clientY });
+    }
     window.addEventListener('pointermove', handleMove);
     return () => window.removeEventListener('pointermove', handleMove);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [canMove]);
 
   return (
     <>
