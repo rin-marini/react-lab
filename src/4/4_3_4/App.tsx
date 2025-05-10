55ea36885632e46885ac5a08cc936eb90b8a0f44
Соколова Мария <rin-marini@yandex.ru>
2025-05-10 15:49:14 +0500

    4_3_4 Fix: corrected data fetching logic inside useEffect

--- src/4/4_3_4/App.tsx
+++ src/4/4_3_4/App.tsx
@@ -1,12 +1,3 @@
-// 4_3_4 Fix fetching inside an Effect
-/*
-  Этот компонент показывает биографию для выбранного человека. Он загружает биографию, вызывая асинхронную функцию fetchBio(person) при монтировании и при каждом изменении person. Эта асинхронная функция возвращает Promise, который в конечном итоге разрешается в строку. Когда выборка завершена, вызывается setBio для отображения этой строки в поле выбора.
-
-  В этом коде есть ошибка. Начните с выбора "Алисы". Затем выберите "Боб" и сразу после этого выберите "Тейлор". Если вы сделаете это достаточно быстро, вы заметите эту ошибку: Тейлор выбран, но в абзаце ниже написано "Это биография Боба".
-
-  Почему так происходит? Исправьте ошибку внутри этого Эффекта.
-*/
-
 import { useState, useEffect } from 'react';
 import { fetchBio } from './api.ts';
 
@@ -15,10 +6,18 @@ export default function Page() {
   const [bio, setBio] = useState<string | null> (null);
 
   useEffect(() => {
+    let ignore = false;
     setBio(null);
+  
     fetchBio(person).then(result => {
-      setBio(result);
+      if (!ignore) {
+        setBio(result);
+      }
     });
+  
+    return () => {
+      ignore = true;
+    };
   }, [person]);
 
   return (
