6cd315374598008dc11c006c082f399aa0434c48
Соколова Мария <rin-marini@yandex.ru>
2025-05-10 21:14:29 +0500

    4_5_2 Fix: switch synchronization

--- src/4/4_5_2/App.tsx
+++ src/4/4_5_2/App.tsx
@@ -1,10 +1,3 @@
-// 4_5_2 Switch synchronization on and off 
-/*
-  В этом примере Эффект подписывается на событие window pointermove, чтобы переместить розовую точку на экране. Попробуйте навести курсор на область предварительного просмотра (или коснуться экрана, если вы пользуетесь мобильным устройством) и посмотрите, как розовая точка следует за вашим движением.
-
-  Также имеется флажок. Установка флажка переключает переменную состояния canMove, но эта переменная состояния не используется нигде в коде. Ваша задача - изменить код так, чтобы при значении canMove, равном false (флажок снят), точка переставала двигаться. После того, как вы снова включите флажок (и установите canMove в true), точка снова должна следовать за движением. Другими словами, то, может ли точка двигаться или нет, должно синхронизироваться с тем, установлен ли флажок.
-*/
-
 import { useState, useEffect } from 'react';
 
 export default function App() {
@@ -12,12 +5,13 @@ export default function App() {
   const [canMove, setCanMove] = useState(true);
 
   useEffect(() => {
-    function handleMove(e) {
-      setPosition({ x: e.clientX, y: e.clientY });
+    function handleMove(e: PointerEvent) {
+      if (canMove)
+        setPosition({ x: e.clientX, y: e.clientY });
     }
     window.addEventListener('pointermove', handleMove);
     return () => window.removeEventListener('pointermove', handleMove);
-  }, []);
+  }, [canMove]);
 
   return (
     <>
