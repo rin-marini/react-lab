0c899521084a2e28352e0c3567925802cdb0ed45
Соколова Мария <rin-marini@yandex.ru>
2025-05-10 12:52:40 +0500

    4_1_2 Fix: resolved component not re-rendering issue

--- src/4/4_1_2/App.tsx
+++ src/4/4_1_2/App.tsx
@@ -1,17 +1,13 @@
-// 4_1_2  Fix a component failing to re-render
-/*
-  Эта кнопка должна переключаться между отображением "Вкл" и "Выкл". Однако она всегда показывает "Выкл". Что не так с этим кодом? Исправьте это.
-*/
-import { useRef } from 'react';
+import { useState } from 'react';
 
 export default function Toggle() {
-  const isOnRef = useRef(false);
+  const [isOn, setIsOn] = useState(false);
 
   return (
     <button onClick={() => {
-      isOnRef.current = !isOnRef.current;
+      setIsOn(!isOn);
     }}>
-      {isOnRef.current ? 'Вкл' : 'Выкл'}
+      {isOn ? 'Вкл' : 'Выкл'}
     </button>
   );
 }
