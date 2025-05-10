0673680820fd36155c0d617c2907c84f5f23900d
Соколова Мария <rin-marini@yandex.ru>
2025-05-10 14:57:35 +0500

    4_3_1 Feat: set focus to input field on component mount

--- src/4/4_3_1/MyInput.tsx
+++ src/4/4_3_1/MyInput.tsx
@@ -1,24 +1,23 @@
 import { useEffect, useRef } from 'react';
 
-export default function MyInput(
-    {
-        value,
-        onChange
-    }: {
-        value: string,
-        onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
-    }) {
+export default function MyInput({
+  value,
+  onChange
+}: {
+  value: string,
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+}) {
+  const ref = useRef<HTMLInputElement>(null);
 
-    const ref = useRef(null);
+  useEffect(() => {
+    ref.current?.focus();
+  }, []);  
 
-    // TODO: This doesn't quite work. Fix it.
-    // ref.current.focus()    
-
-    return (
-        <input
-            ref={ref}
-            value={value}
-            onChange={onChange}
-        />
-    );
+  return (
+    <input
+      ref={ref}
+      value={value}
+      onChange={onChange}
+    />
+  );
 }
