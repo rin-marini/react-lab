9d61ec14641611f3d07d88bff385437a418404d5
Соколова Мария <rin-marini@yandex.ru>
2025-05-10 20:03:13 +0500

    4_4_2 Refactor: replaced useEffect with useMemo to cache visibleTodos

--- src/4/4_4_2/App.tsx
+++ src/4/4_4_2/App.tsx
@@ -1,21 +1,14 @@
-// 4_4_2 Cache a calculation without Effects 
-/*
-  В этом примере фильтрация тодосов была вынесена в отдельную функцию под названием getVisibleTodos(). Эта функция содержит внутри себя вызов console.log(), который поможет вам заметить, когда она вызывается. Установите флажок "Показывать только активные тодосы" и обратите внимание, что это вызывает повторный запуск getVisibleTodos(). Это ожидаемо, поскольку видимые тодосы меняются, когда вы переключаете, какие из них показывать.
-
-  Ваша задача - удалить эффект, который пересчитывает список visibleTodos в компоненте TodoList. Однако, вам нужно убедиться, что getVisibleTodos() не повторно запускается (и поэтому не печатает никаких логов), когда вы вводите данные в input.
-*/
-
-import { useState, useEffect } from 'react';
+import { useState, useMemo } from 'react';
 import { initialTodos, createTodo, getVisibleTodos, Todo } from './todos.js';
+
 
 export default function TodoList() {
   const [todos, setTodos] = useState(initialTodos);
   const [showActive, setShowActive] = useState(false);
   const [text, setText] = useState('');
-  const [visibleTodos, setVisibleTodos] = useState<Todo[]>([]);
 
-  useEffect(() => {
-    setVisibleTodos(getVisibleTodos(todos, showActive));
+  const visibleTodos = useMemo(() => {
+    return getVisibleTodos(todos, showActive);
   }, [todos, showActive]);
 
   function handleAddClick() {
--- src/4/4_4_2/todos.ts
+++ src/4/4_4_2/todos.ts
@@ -4,27 +4,26 @@ export type Todo = {
   completed: boolean;
 };
 
-
 let nextId = 0;
 let calls = 0;
 
 export function getVisibleTodos(todos: Todo[], showActive: boolean) {
-console.log(`getVisibleTodos() was called ${++calls} times`);
-const activeTodos = todos.filter(todo => !todo.completed);
-const visibleTodos = showActive ? activeTodos : todos;
-return visibleTodos;
+  console.log(`getVisibleTodos() was called ${++calls} times`);
+  const activeTodos = todos.filter(todo => !todo.completed);
+  const visibleTodos = showActive ? activeTodos : todos;
+  return visibleTodos;
 }
 
 export function createTodo(text: string, completed = false): Todo {
-return {
-  id: nextId++,
-  text,
-  completed
-};
+  return {
+    id: nextId++,
+    text,
+    completed
+  };
 }
 
 export const initialTodos: Todo[] = [
-createTodo('Get apples', true),
-createTodo('Get oranges', true),
-createTodo('Get carrots'),
+  createTodo('Get apples', true),
+  createTodo('Get oranges', true),
+  createTodo('Get carrots')
 ];
